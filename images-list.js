c479e9e50da2d6a2ca1256b4622e5d94e973188c
ssyahia-source <ssyahia@gmail.com>
2026-01-02 12:01:07 +0100

    Add files via upload

---
+++ images-list.js
@@ -0,0 +1,19 @@
+// images-list.js
+const IMAGES_LIST = [
+    'nuage.jpg',
+    'paris pistache.jpg', 
+    'tarte fruit Rouge.jpg',
+    'cheescake vanille.jpg',
+    'opera.jpg',
+    'Cookies double.jpg',
+    'Cookies croquant.jpg',
+    'Cookies choco.jpg',
+    'Cookies avoine.jpg',
+    'fondant.jpg',
+    'truff fondant.jpg',
+    'caramel et lait.jpg',
+    'fromboise.jpg',
+    'le coeur.jpg',
+    'fond.jpg',
+    'fond2.jpg'
+];
